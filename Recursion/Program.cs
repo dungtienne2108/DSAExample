8169d94e944a0ca8c2039574cc7daad67f72a763
Neit2108 <tienld.22810310329@epu.edu.vn>
2025-07-06 23:52:45 +0700

    Bai 15 : De quy

---
+++ Recursion/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Recursion
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Tìm số fibonacci thứ n.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static long Fibonacci(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Nhập n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Fibonacci thứ {n} là: {Fibonacci(n)}");
+        }
+    }
+}
